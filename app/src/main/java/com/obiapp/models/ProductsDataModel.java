7563a2764f6741a9aa3df01b85bb49f21b2d7457
EL MOTWRON <ahmedmohamed23345@gmail.com>
2021-02-10 16:39:03 +0200

    ch

--- app/src/main/java/com/obiapp/models/ProductsDataModel.java
+++ app/src/main/java/com/obiapp/models/ProductsDataModel.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class ProductsDataModel extends StatusResponse implements Serializable {
     private List<ProductModel> data;
+    private int status;
 
     public List<ProductModel> getData() {
         return data;
+    }
+
+
+    public int getStatus() {
+        return status;
     }
 }
